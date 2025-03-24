341b40e63ee615b3a7bfbb193aa16a9b1be53820
Joseph <sourpina@gmail.com>
2025-03-24 18:36:44 -0400

    Added Deck class

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public class Deck
+{
+    [field: SerializeField] public Card[] Cards { get; private set; }
+    [SerializeField] private int _topIndex;
+
+    public Deck()
+    {
+        Cards = new Card[52];
+        _topIndex = 0;
+
+        int index = 0;
+        foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                Cards[index++] = new Card(rank, suit);
+    }
+
+    public void Shuffle()
+    {
+        for (int i = 0; i < 52; i++)
+        {
+            int r = Random.Range(i, 52);
+            Card temp = Cards[i];
+            Cards[i] = Cards[r];
+            Cards[r] = temp;
+        }
+    }
+
+    public Card TakeTopCard()
+    {
+        if (_topIndex >= 52)
+            return null;
+
+        return Cards[_topIndex++];
+    }
+}
