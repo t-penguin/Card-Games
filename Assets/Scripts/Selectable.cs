59831af7a02b6b0be0e79407bd5e12778ef87d06
Joseph <sourpina@gmail.com>
2025-03-25 01:13:46 -0400

    Added Selectable class
    Selectables are scene objects that can be hovered over and clicked when active and will have a visible outline when doing so

---
+++ Assets/Scripts/Selectable.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Selectable : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [SerializeField] private GameObject _selector;
+
+    public bool Selected { get; private set; }
+    public bool Active { get; set; }
+
+    #region Pointer Event Callbacks
+
+    public virtual void OnPointerClick(PointerEventData eventData)
+    {
+        if (!Active)
+            return;
+
+        Selected = !Selected;
+        if (Selected)
+            ShowSelector();
+        else
+            HideSelector();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!Active || Selected)
+            return;
+
+        ShowSelector();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!Active || Selected)
+            return;
+
+        HideSelector();
+    }
+
+    #endregion
+
+    private void ShowSelector() => _selector.SetActive(true);
+    private void HideSelector() => _selector.SetActive(false);
+
+    public void Deselect()
+    {
+        Selected = false;
+        HideSelector();
+    }
+}
